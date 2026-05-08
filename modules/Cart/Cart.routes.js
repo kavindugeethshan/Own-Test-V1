9c305ddb1cf1906f8ef5624270a7cad9f3ceb07e
kavindugeethshan <k.geethshan@gmail.com>
2026-05-08 16:21:10 +0530

    Update-Cart.rotes.js
    remove items 🤷‍♀️🤷‍♀️🤷‍♀️🤷‍♀️🤷‍♀️

--- modules/Cart/Cart.routes.js
+++ modules/Cart/Cart.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addToCartController ,getUserCartController} from "./Cart.Controllers.js";
+import { addToCartController ,getUserCartController,removeFromCartController} from "./Cart.Controllers.js";
 import authenticate from "../../middlewares/authenticate.js";
 const router = express.Router();
 
@@ -13,5 +13,9 @@ router.get("/", authenticate, getUserCartController);
 // Access: Private (Requires Token)
 router.post("/add", authenticate, addToCartController);
 
+// Route: DELETE /cart/remove/:productCode
+// Description: Remove an item from the user's cart
+// Access: Private (Requires Token)
+router.delete("/remove/:productCode", authenticate, removeFromCartController);
 
 export default router;
